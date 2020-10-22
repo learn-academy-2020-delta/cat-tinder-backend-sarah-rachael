55a4bda04a58b5ce6d7f7b78bc8915a7c9f790bd
sjproctor <sproctor950@gmail.com>
2020-10-22 13:07:57 -0700

    adds api enpoints, validations, cors, testing

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should validate name' do
+    cat = Cat.create(age: 2, enjoys: 'Walks in the park')
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it 'should validate age' do
+    cat = Cat.create(name: 'Felix', enjoys: 'Walks in the park')
+    expect(cat.errors[:age]).to_not be_empty
+  end
+
+  it 'should validate enjoys' do
+    cat = Cat.create(name: 'Felix', age: 2)
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it 'should have an enjoys attribute with at least 10 characters' do
+    cat = Cat.create(name: 'Felix', age: 7, enjoys: 'Walks')
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
 end
--- spec/requests/cats_request_spec.rb
+++ spec/requests/cats_request_spec.rb
@@ -2,33 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'Cats', type: :request do
 
-  it 'gets a list of Cats' do
-    # Create a new cat in the Test Database (not the same one as development)
+  cat_params = {
+    cat: {
+      name: 'Buster',
+      age: 4,
+      enjoys: 'Meow Mix, and plenty of sunshine.'
+    }
+  }
+
+  it "gets a list of Cats" do
+    # Create a new cat in the Test Database (this is not the same one as development)
     Cat.create(name: 'Felix', age: 2, enjoys: 'Walks in the park')
 
     # Make a request to the API
     get '/cats'
 
     # Convert the response into a Ruby Hash
-    cats = JSON.parse(response.body)
+    json = JSON.parse(response.body)
 
     # Assure that we got a successful response
     expect(response).to have_http_status(200)
 
     # Assure that we got one result back as expected
-    expect(cats.length).to eq 1
+    expect(json.length).to eq 1
   end
 
-  it 'creates a new Cat' do
-    # The params we are going to send with the request
-    cat_params = {
-      cat: {
-        name: 'Buster',
-        age: 4,
-        enjoys: 'Meow Mix, and plenty of sunshine.'
-      }
-    }
-
+  it "creates a cat" do
     # Send the request to the server
     post '/cats', params: cat_params
 
@@ -40,55 +39,44 @@ RSpec.describe 'Cats', type: :request do
 
     # Assure that the created cat has the correct attributes
     expect(cat.name).to eq 'Buster'
+    expect(cat.age).to eq 4
+    expect(cat.enjoys).to eq 'Meow Mix, and plenty of sunshine.'
   end
 
-
-  it 'edits a cat' do
-    cat_params = {
-      cat: {
-        name: 'Felix',
-        age: 4,
-        enjoys: 'Walks in the park.'
-      }
-    }
+  it "edits a cat" do
+    # Send the request to the server
     post '/cats', params: cat_params
 
     cat = Cat.first
+    expect(cat.age).to eq 4
 
     new_cat_params = {
       cat: {
-        name: 'Felix',
-        age: 2,
-        enjoys: 'Walks in the park.'
+        name: 'Buster',
+        age: 7,
+        enjoys: 'Meow Mix, and plenty of sunshine.'
       }
     }
+
     patch "/cats/#{cat.id}", params: new_cat_params
-
-    # redefine the variable to reference the same cat initially created
-    cat = Cat.find cat.id
-
-    # Assure that we get a success back
+    cat = Cat.find(cat.id)
     expect(response).to have_http_status(200)
-
-    # Assure that the edited cat has the correct attributes
-    expect(cat.age).to eq 2
+    expect(cat.age).to eq 7
   end
 
 
-  it 'deletes a cat' do
-    cat_params = {
-      cat: {
-        name: 'Felix',
-        age: 4,
-        enjoys: 'Walks in the park.'
-      }
-    }
+  it "destroys a cat" do
+    # Send the request to the server
     post '/cats', params: cat_params
+
     cat = Cat.first
+
     delete "/cats/#{cat.id}"
+
     expect(response).to have_http_status(200)
-    puts cat.valid?
-  end
+    cat = Cat.all
+    expect(cat).to be_empty
+end
 
 
 
@@ -107,6 +95,32 @@ RSpec.describe 'Cats', type: :request do
     json = JSON.parse(response.body)
     # Errors are returned as an array because there could be more than one, if there are more than one validation failures on an attribute.
     expect(json['name']).to include "can't be blank"
+  end
+
+  it "doesn't create a cat without an age" do
+    cat_params = {
+      cat: {
+        name: 'Felix',
+        enjoys: 'Walks in the park'
+      }
+    }
+    post '/cats', params: cat_params
+    expect(response.status).to eq 422
+    json = JSON.parse(response.body)
+    expect(json['age']).to include "can't be blank"
+  end
+
+  it "doesn't create a cat without enjoys" do
+    cat_params = {
+      cat: {
+        name: 'Felix',
+        age: 3
+      }
+    }
+    post '/cats', params: cat_params
+    expect(response.status).to eq 422
+    json = JSON.parse(response.body)
+    expect(json['enjoys']).to include "can't be blank"
   end
 
 end
